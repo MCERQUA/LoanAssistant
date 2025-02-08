505b351aaba0e674d5091d0636f20c2db7c0ec1a
MCerqua <55824402+MCERQUA@users.noreply.github.com>
2025-02-08 04:19:34 -0500

    AI Loan Assistant App Update

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,12 @@ function App() {
         </div>
       </div>
       
-      {/* Main Content Area */}
-      <div className="flex-1 overflow-hidden flex flex-col">
-        <div className="max-w-4xl w-full mx-auto flex-1 flex flex-col items-center">
-          {/* Image Container - Square aspect ratio */}
-          <div className="w-[75%] pt-3">
-            <div className="aspect-square w-full rounded-lg shadow-lg overflow-hidden">
+      {/* Main Content Area - Added pb-20 to account for bottom menu */}
+      <div className="flex-1 overflow-hidden flex flex-col px-3 pb-20">
+        <div className="max-w-4xl w-full mx-auto flex flex-col items-center h-full">
+          {/* Image Container */}
+          <div className="w-full max-w-2xl pt-4 pb-2">
+            <div className="w-full h-64 rounded-lg shadow-lg overflow-hidden">
               <img 
                 src="/Morty-Gauge.jpg" 
                 alt="Morty Gauge" 
@@ -29,8 +29,8 @@ function App() {
             </div>
           </div>
           
-          {/* Chat Window - Takes remaining height with padding */}
-          <div className="flex-1 min-h-0 w-full px-3 pt-3">
+          {/* Chat Window - Fixed height */}
+          <div className="w-full h-[calc(100vh-20rem)]">
             <ChatWindow />
           </div>
         </div>
